4946c5ef0766ba26960913eb471dc22d443f126f
Roger Hamilton <RobertGulbahce@users.noreply.github.com>
2025-05-25 11:27:25 +1000

    Update post-slack-chart.js

--- api/post-slack-chart.js
+++ api/post-slack-chart.js
@@ -163,7 +163,7 @@ export default async function handler(req, res) {
         elements: [
           {
             type: "mrkdwn",
-            text: `_Responsibility: ${owner}_`
+            text: `*Responsibility:* ${owner}`
           }
         ]
       },
